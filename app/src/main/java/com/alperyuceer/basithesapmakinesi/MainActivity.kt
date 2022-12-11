48efe81d304fe05c0951ba53db75ecff3bd299ba
alperyuceer <yuceeralper06@gmail.com>
2022-12-11 15:10:33 +0300

    Basit fonksiyonlar ve basit tasarım yapıldı.

--- app/src/main/java/com/alperyuceer/basithesapmakinesi/MainActivity.kt
+++ app/src/main/java/com/alperyuceer/basithesapmakinesi/MainActivity.kt
@@ -2,11 +2,34 @@ package com.alperyuceer.basithesapmakinesi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.alperyuceer.basithesapmakinesi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("Hesap Makinesi")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
+    fun topla(view: View){
+        var sonuc= binding.number1.text.toString().toInt() + binding.number2.text.toString().toInt()
+        binding.sonuc.text = "SONUÇ: ${sonuc}"
+    }
+    fun cikar(view: View){
+        var sonuc= binding.number1.text.toString().toInt() - binding.number2.text.toString().toInt()
+        binding.sonuc.text = "SONUÇ: ${sonuc}"
+    }
+    fun carp(view: View){
+        var sonuc= binding.number1.text.toString().toInt() * binding.number2.text.toString().toInt()
+        binding.sonuc.text = "SONUÇ: ${sonuc}"
+    }
+    fun bol(view: View){
+        var sonuc= binding.number1.text.toString().toDouble() / binding.number2.text.toString().toDouble()
+        binding.sonuc.text = "SONUÇ: ${sonuc}"
+    }
+
 }
